e9aa924f55c242f7d76de60366706f7ad9cfac2d
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-25 17:54:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(seconds_to_midnight)
   puts "#{seconds_to_midnight} SECOND(S)!"
   seconds_to_midnight -= 1
   end
-seconds_to_midnight.to_s + " left.HAPPY NEW YEAR!"
+seconds_to_midnight.to_s + "HAPPY NEW YEAR!"
 end
 
 
